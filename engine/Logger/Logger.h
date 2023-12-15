74a5c7eb62c92fdccd6f5b0aa1249d1cb478449d
AutoPilot <alenandreasian99@gmail.com>
2023-12-15 23:19:48 +0200

    logger macro modification

--- engine/Logger/Logger.h
+++ engine/Logger/Logger.h
@@ -3,7 +3,7 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
-#define ENGINE_NAME "ENGINE"
+#define ENGINE_NAME      "ENGINE"
 #define APPLICATION_NAME "APPLICATION"
 
 #ifdef ENGINE
@@ -12,11 +12,11 @@
 	#define GET_LOG() spdlog::get(APPLICATION_NAME)
 #endif
 
-#define TRACE_LOG(x, ...)     GET_LOG()->trace(x, __VA_ARGS__);
-#define INFO_LOG(x, ...)      GET_LOG()->info(x, __VA_ARGS__);
-#define WARN_LOG(x, ...)      GET_LOG()->warn(x, __VA_ARGS__);
-#define ERROR_LOG(x, ...)     GET_LOG()->error(x, __VA_ARGS__);
-#define CRITICAL_LOG(x, ...)  GET_LOG()->critical(x, __VA_ARGS__);
+#define TRACE_LOG(x, ...)     GET_LOG()->trace(x,##__VA_ARGS__);
+#define INFO_LOG(x, ...)      GET_LOG()->info(x,##__VA_ARGS__);
+#define WARN_LOG(x, ...)      GET_LOG()->warn(x,##__VA_ARGS__);
+#define ERROR_LOG(x, ...)     GET_LOG()->error(x,##__VA_ARGS__);
+#define CRITICAL_LOG(x, ...)  GET_LOG()->critical(x,##__VA_ARGS__);
 
 namespace engine
 {
